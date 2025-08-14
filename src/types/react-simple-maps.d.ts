13e43de65748c037f49708110a74735413373415
Authentic <seadiscord@gmail.com>
2025-08-14 22:42:57 +1000

    Add TypeScript declarations for react-simple-maps

---
+++ src/types/react-simple-maps.d.ts
@@ -0,0 +1,53 @@
+declare module 'react-simple-maps' {
+  import { ComponentType, ReactNode } from 'react';
+
+  export interface ComposableMapProps {
+    projection?: string;
+    projectionConfig?: {
+      scale?: number;
+      center?: [number, number];
+      rotation?: [number, number, number];
+    };
+    width?: number;
+    height?: number;
+    className?: string;
+    style?: React.CSSProperties;
+    children?: ReactNode;
+  }
+
+  export interface GeographiesProps {
+    geography: string;
+    children: (geographies: any[]) => ReactNode;
+  }
+
+  export interface GeographyProps {
+    geography: any;
+    onMouseEnter?: (event: React.MouseEvent) => void;
+    onMouseLeave?: (event: React.MouseEvent) => void;
+    onClick?: (event: React.MouseEvent) => void;
+    style?: {
+      default?: React.CSSProperties;
+      hover?: React.CSSProperties;
+      pressed?: React.CSSProperties;
+    };
+    fill?: string;
+    stroke?: string;
+    strokeWidth?: number;
+    className?: string;
+  }
+
+  export interface ZoomableGroupProps {
+    zoom?: number;
+    center?: [number, number];
+    onMoveEnd?: (center: [number, number], zoom: number) => void;
+    minZoom?: number;
+    maxZoom?: number;
+    className?: string;
+    children?: ReactNode;
+  }
+
+  export const ComposableMap: ComponentType<ComposableMapProps>;
+  export const Geographies: ComponentType<GeographiesProps>;
+  export const Geography: ComponentType<GeographyProps>;
+  export const ZoomableGroup: ComponentType<ZoomableGroupProps>;
+}
